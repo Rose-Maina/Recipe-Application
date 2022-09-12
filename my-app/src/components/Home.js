3728398524bff27d332dd75b91f54baf04de388c
Rose Maina <rosewairimu27@gmail.com>
2022-09-12 08:54:59 +0300

    Creating the footer div

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -49,8 +49,7 @@ const Home = () => {
             {mealRecipes}
       </div>
       <div class="footer">
-        <p>Copyright &copy; myrecipes.com</p>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjSEo__EXpP_zd581Ds_X9HwwbXg_fRPIo6A&usqp=CAU" alt="logo" />     
+        
       </div>
   </div>
   </div>
